f95b885e2f62cbd54724e013e04ca255c4c3ce6f
Stewan <oi@stewan.io>
2018-02-14 17:14:26 -0200

    feat(): adiciona aula 9

---
+++ super-store/src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+// modulos
+import { RoutingModule } from './app.routes';
+
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
+import { CartbarComponent } from './components/cartbar/cartbar.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
+import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { Error404PageComponent } from './pages/error-404-page/error-404-page.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    MenuComponent,
+    LogoComponent,
+    SearchbarComponent,
+    CartbarComponent,
+    BannerComponent,
+    ProductListComponent,
+    ProductCardComponent,
+    FooterComponent,
+    HomePageComponent,
+    ProductsPageComponent,
+    ProductPageComponent,
+    Error404PageComponent
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ super-store/src/app/pages/error-404-page/error-404-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-error-404-page',
+  templateUrl: './error-404-page.component.html',
+  styleUrls: ['./error-404-page.component.sass']
+})
+export class Error404PageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
