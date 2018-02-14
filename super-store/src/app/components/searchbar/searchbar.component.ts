b81bc175b306038f0ca69697275a699be3839ea0
Stewan <oi@stewan.io>
2018-02-14 17:29:04 -0200

    feat(): adiciona aula 12

---
+++ super-store/src/app/components/searchbar/searchbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchbar',
+  templateUrl: './searchbar.component.html',
+  styleUrls: ['./searchbar.component.sass']
+})
+export class SearchbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ super-store/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuService } from "app/providers/menu.service";
+
+@Component({
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.sass']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor(public menu: MenuService) { }
+
+  ngOnInit() {
+  }
+
+}
